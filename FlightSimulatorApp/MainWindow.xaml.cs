0d418775341ca701b5dc520c08001b7f3359a43a
kfiryehuda <kfir.yehuda50@gmail.com>
2020-03-19 13:06:37 +0200

    Add project files.

---
+++ FlightSimulatorApp/MainWindow.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Forms;
+
+namespace FlightSimulatorApp
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        //private Point firstPoint = new Point();
+        //private void joy_MouseDown(object sender, MouseButtonEventArgs e)
+        //{
+        //    if(e.ChangedButton == MouseButton.Left) { firstPoint = e.GetPosition(this); }
+        //}
+        
+        //private void joy_MouseMove(object sender, System.Windows.Input.MouseEventArgs e)
+        //{
+        //    if (e.LeftButton == MouseButtonState.Pressed)
+        //    {
+        //        //joy.knobPosition.X = e.GetPosition(this).X + joy.knobPosition.X - firstPoint.X;
+        //        //joy.knobPosition.Y = e.GetPosition(this).Y + joy.knobPosition.Y - firstPoint.Y;
+        //        joy.knobPosition.X = e.GetPosition(this).X - firstPoint.X;
+        //        joy.knobPosition.Y = e.GetPosition(this).Y - firstPoint.Y;
+                
+        //    }
+        //}
+    }
+}
---
+++ FlightSimulatorApp/Views/joistick.xaml.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace FlightSimulatorApp.Views
+{
+    /// <summary>
+    /// Interaction logic for joistick.xaml
+    /// </summary>
+    public partial class joistick : UserControl
+    {
+        public joistick()
+        {
+            InitializeComponent();
+        }
+        private void centerKnob_Completed(object sender, EventArgs e) { }
+        private Point firstPoint = new Point();
+        private void Knob_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.ChangedButton == MouseButton.Left) { firstPoint = e.GetPosition(this); }
+        }
+
+        private void Knob_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (e.LeftButton == MouseButtonState.Pressed)
+            {
+                double x = e.GetPosition(this).X - firstPoint.X;
+                double y = e.GetPosition(this).Y - firstPoint.Y;
+                if (Math.Sqrt(x*x + y*y) < Base.Width / 2)
+                {
+                    knobPosition.X = x;
+                    knobPosition.Y = y;
+                }
+                
+                
+
+            }
+        }
+
+        private void Knob_MouseUp(object sender, MouseButtonEventArgs e)
+        {
+            knobPosition.X = 0;
+            knobPosition.Y = 0;
+        }
+
+        private void Ellipse_MouseLeave(object sender, MouseEventArgs e)
+        {
+        }
+
+        private void Ellipse_MouseEnter(object sender, MouseEventArgs e)
+        {
+        }
+    }
+}
