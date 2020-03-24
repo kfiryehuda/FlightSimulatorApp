753d7f7b9352d73b6086fc6dbb2114221b089f0a
benaz <benazoulaydev@gmail.com>
2020-03-24 13:37:39 +0200

    server v1

--- FlightSimulatorApp/Client.cs
+++ FlightSimulatorApp/Client.cs
@@ -4,28 +4,95 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+
+using System.Net;
+using System.Net.Sockets;
+
+using System.IO;
+
+
 namespace FlightSimulatorApp
 {
+
     class Client : IClient
     {
+
+        TcpClient tcpClient = new TcpClient();
+        NetworkStream netStream;
         public void connect(string ip, int port)
         {
-            throw new NotImplementedException();
+
+            try
+            {
+                Console.WriteLine("Connecting.....");
+                tcpClient.Connect(ip, port);
+                // use the ipaddress as in the server program
+                Console.WriteLine("Connected");
+
+                String str = Console.ReadLine();
+                netStream = tcpClient.GetStream();
+
+            }
+
+            catch (Exception e)
+            {
+                Console.WriteLine("Error..... " + e.StackTrace);
+            }
         }
 
         public void disconnect()
         {
-            throw new NotImplementedException();
+            netStream.Close();
+            tcpClient.Close();
         }
 
         public string read()
         {
-            throw new NotImplementedException();
+           if (netStream.CanRead)
+            {
+                // Reads NetworkStream into a byte buffer.
+                byte[] bytes = new byte[tcpClient.ReceiveBufferSize];
+
+                // Read can return anything from 0 to numBytesToRead. 
+                // This method blocks until at least one byte is read.
+                netStream.Read(bytes, 0, (int)tcpClient.ReceiveBufferSize);
+
+                // Returns the data received from the host to the console.
+                string returndata = Encoding.UTF8.GetString(bytes);
+
+                Console.WriteLine("This is what the host returned to you: " + returndata);
+                return returndata;
+            }
+            else
+            {
+                Console.WriteLine("You cannot read data from this stream.");
+                tcpClient.Close();
+
+                // Closing the tcpClient instance does not close the network stream.
+                netStream.Close();
+                return "";
+            }
         }
 
         public void write(string command)
         {
-            throw new NotImplementedException();
+            if (netStream.CanWrite)
+            {
+                Byte[] sendBytes = Encoding.UTF8.GetBytes(command);
+                netStream.Write(sendBytes, 0, sendBytes.Length);
+
+            }
+            else
+            {
+                Console.WriteLine("You cannot write data to this stream.");
+                tcpClient.Close();
+
+                // Closing the tcpClient instance does not close the network stream.
+                netStream.Close();
+                return;
+            }
+
+
         }
     }
 }
